7d87488be2405b065ed6374d8e4da4bc98dde8e3
jungmin0917 <cloonds@gmail.com>
2023-04-26 18:00:32 +0900

    스프링부트 쇼핑몰 책 2장 거의 끝

--- shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,8 +15,22 @@ import java.util.List;
 // DAO의 역할로 사용할 Repository 선언함
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    // 상품명으로 데이터를 조회하는 메소드 선언 (같은 상품명이 여러 개 있을 수 있으므로 List를 사용함)
+    // 상품명으로 데이터를 조회 (반환값이 여러 개 있을 수 있으므로 List를 사용함)
     List<Item> findByItemNm(String itemNm);
+    
+    // 상품명 및 상품 상세 설명으로 데이터 조회
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
+    // 상품 가격보다 값이 작은 데이터 조회
+    List<Item> findByPriceLessThan(Integer price); // 왜 그런지 모르겠는데 int가 아니고 래퍼 클래스를 썼다.
 
+    // 상품 가격보다 값이 작은 데이터 조회인데 가격을 기준으로 내림차순
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("SELECT i FROM Item i WHERE i.itemDetail LIKE %:itemDetail% ORDER BY i.price DESC")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail); // 파라미터로 itemDetail을 받고 그것을 @Query 내부에서 :변수명 대신에 넣겠다는 뜻
+    // Item i 로 Item 엔티티를 현재 쿼리에서 i라는 별칭으로 쓸 것임을 명시했다.
+    // 여기서 i는 Item 엔티티의 별칭(alias)로 사용되고 있다.
+    // JPQL에서는 엔티티와 연관된 데이터를 조회할 때, 엔티티에 대한 별칭을 사용할 수도 있다. (안 사용해도 됨)
+    // 위와 같이 @Query가 달린 메소드는 그냥 바로 쿼리메소드 식으로 해석해서 실행하는 것이 아니고, 개발자가 @Query 내부에 적은 JPQL대로 실행한다.
 }
--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,6 +22,9 @@ class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository; // 테스트할 Repository 인터페이스 주입받음
+
+    @PersistenceContext // 엔티티 매니저를 컨테이너가 자동으로 생성하고 주입하도록 함
+    EntityManager em; // 직접 생성하지 않으므로 의존성 관리가 편하다
 
     @Test // 메소드 테스트 어노테이션
     @DisplayName("상품 저장 테스트") // 테스트명 지정
@@ -50,10 +58,82 @@ class ItemRepositoryTest {
         for(Item item : itemList){
             System.out.println(item.toString()); // 해당 아이템의 전체 정보 출력
         }
-
-
     }
 
+    @Test
+    @DisplayName("상품명 및 상품상세설명 조회 (Or) 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemTest(); // 일단 상품 여러 개 생성
+
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격보다 작은 데이터 조회 (LessThan) 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemTest(); // 일단 상품 만듦
+
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격보다 작은 데이터를 가격 기준 내림차순으로 조회 (OrderBy) 테스트")
+    public void findByPriceLessThanOrderByPriceDescTest(){
+        this.createItemTest();
+
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemTest(); // 일단 상품 등록함
+
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemTest();
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); // 주입받은 EntityManger로 JPAQueryFactory를 생성함. 쿼리를 동적으로 생성하기 위한 팩토리 객체
+
+        QItem qItem = QItem.item; // 쿼리를 생성하기 위해 플러그인을 통해 자동으로 생성된 QItem 객체를 이용한다. (일반 Item 엔티티 객체로는 쿼리 동적 생성이 안 되므로 주의할 것)
+
+        // PHP에서 했던 것처럼 .select.where.where.orderBy 이렇게 여러 쿼리를 이어서 쓸 수 있다
+        // 즉 평소엔 메소드 쿼리 쓰고, 메소드 쿼리를 사용하기엔 복잡한 로직의 경우 Querydsl을 이용하면 될 것 같다.
+        // JPAQuery<엔티티명> 쿼리명 = queryFactory.각종 쿼리 메소드().각종 쿼리 메소드();
+        JPAQuery<Item> query = queryFactory
+                .selectFrom(qItem) // Item 엔티티에 대한 메타모델 정보를 담고 있는 qItem에서 찾음
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL)) // itemSellStatus가 SELL인 것을 찾음
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        // 위와 같이 JPAQuery 객체를 만들었으면 이걸 fetch 한다
+
+        List<Item> itemList = query.fetch(); // fetchAll하지 않고 그냥 fetch해도 정보가 전부 담김 (웬만하면 fetchAll() 대신 fetch() 사용할 것). 반환값이 List 자료형으로 반환된다
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 }
 
 
